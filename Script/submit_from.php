6e84b20afeb611ed95c2d12a75b66f479e85147e
Askhat128 <askhat.baimagambetov128@gmail.com>
2024-12-16 02:28:33 +0700

    fix_07

--- Script/submit_from.php
+++ Script/submit_from.php
@@ -1,10 +1,21 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Получаем данные из формы
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    // Здесь можно добавить отправку на email или сохранение в базу данных
-    echo "Спасибо, $name! Ваше сообщение успешно отправлено.";
+    // Настройка письма
+    $to = "askhat.baimagambetov128@gmail.com"; // Email получателя
+    $subject = "Новое сообщение с сайта Все для строительства дома";
+    $body = "Имя: $name\nEmail: $email\nСообщение:\n$message";
+    $headers = "From: $email";
+
+    // Отправка письма
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Спасибо, $name! Ваше сообщение успешно отправлено.";
+    } else {
+        echo "К сожалению, произошла ошибка. Пожалуйста, попробуйте позже.";
+    }
 }
 ?>
