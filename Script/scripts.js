a8c0cde266a5ade792849f9b83adb18edcbde3eb
Askhat128 <askhat.baimagambetov128@gmail.com>
2024-12-16 03:46:06 +0700

    fix_11

--- Script/scripts.js
+++ Script/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const toTopBtn = document.getElementById("toTopBtn");
 
     window.onscroll = () => {
-        if (window.scrollY > 200) {
+        if (window.scrollY > 100) {
             toTopBtn.style.display = "block";
         } else {
             toTopBtn.style.display = "none";
